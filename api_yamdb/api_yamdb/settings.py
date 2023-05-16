3fcf74801ad643332e052c0c08336ac5ced4eb30
creamsandwch <neuronrus1996@gmail.com>
2023-05-16 11:43:05 +0500

    badge and some changes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = False
 
